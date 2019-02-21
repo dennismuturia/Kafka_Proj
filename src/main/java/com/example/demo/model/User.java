e3d94f988acede439dc4a648f656010fd23f534b
dennis <dmuturia2922@gmail.com>
2019-02-21 13:11:00 +0300

    Making the consumer in the same application in the same application. It will only contain

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,27 @@
 package com.example.demo.model;
 
 public class User {
+    private String fname;
+    private String lname;
+    private int age;
+    //Default constructor
+    public User(String fname, String lname, int age){
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+
+    }
+
+    //Configure all the getters
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
